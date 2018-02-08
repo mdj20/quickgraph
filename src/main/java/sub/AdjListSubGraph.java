1120e99470e1ca0adf0ab95fbc09a7c2a3ee7d50
mdj20 <mdj20@pitt.edu>
2018-02-07 21:10:30 -0500

    push and merge

---
+++ src/main/java/sub/AdjListSubGraph.java
@@ -0,0 +1,13 @@
+package sub;
+
+import java.util.Set;
+
+import com.mdj20.quickgraph.quickgraph.main.AdjListGraph;
+import com.mdj20.quickgraph.quickgraph.main.Edge;
+import com.mdj20.quickgraph.quickgraph.main.UserGraph;
+
+public class AdjListSubGraph<V> extends AbstractSubGraph<UserGraph<V>, V, Edge<V>> implements UserGraph<V> {
+	AdjListSubGraph(UserGraph<V> parentGraph, Set<V> vertices){
+		super(parentGraph,vertices);
+	}
+}
