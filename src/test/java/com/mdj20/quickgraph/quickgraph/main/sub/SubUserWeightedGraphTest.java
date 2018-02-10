ec83baaabd70166383f9d8a22d9040ca083cad9d
Matthew Jeffreys <mdj20@pitt.edu>
2018-02-09 19:00:21 -0500

    added test cases to subuserweightedgraph.java

--- src/test/java/com/mdj20/quickgraph/quickgraph/main/sub/SubUserWeightedGraphTest.java
+++ src/test/java/com/mdj20/quickgraph/quickgraph/main/sub/SubUserWeightedGraphTest.java
@@ -2,12 +2,15 @@ package com.mdj20.quickgraph.quickgraph.main.sub;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
 
+import com.mdj20.quickgraph.quickgraph.main.Edge;
 import com.mdj20.quickgraph.quickgraph.main.WeightedAdjListGraph;
+import com.mdj20.quickgraph.quickgraph.main.WeightedEdge;
 import com.mdj20.quickgraph.quickgraph.testutilities.FastGraphBuilder;
 
 public class SubUserWeightedGraphTest {
@@ -21,12 +24,33 @@ public class SubUserWeightedGraphTest {
 	public void testAddVertex() {
 		int nVert = 10;
 		int subVertexLimit = nVert/2;
+		int added = subVertexLimit+1;
 		WeightedAdjListGraph<Integer,Integer> graph = FastGraphBuilder.createSaturatedWeightedGraph(nVert);
 		HashSet<Integer> subVertex = new HashSet<Integer>();
 		for(int i = 0 ; i < subVertexLimit ; i++) {
 			subVertex.add(i);
 		}
-		SubUserWeightedGraph<Integer,Integer> subGraph = new SubUserWeightedGraph<Integer,Integer>(graph,subVertex);
+		SubUserWeightedGraph<Integer, Integer> subGraph = new SubUserWeightedGraph<Integer,Integer>(graph,subVertex);
+		
+		// Add vertex
+		subGraph.addVertex(added);
+		
+		// assert it was added
+		assertTrue(subGraph.getVertices().contains(added));
+		
+		Set<WeightedEdge<Integer, Integer>> superEdge = graph.getConnectingEdges(added);
+		Set<WeightedEdge<Integer,Integer>> edge = subGraph.getConnectingEdges(added);
+		assertTrue(edge.size()==subVertexLimit);
+		boolean[] expected = new boolean[subVertexLimit], actual = new boolean[subVertexLimit];
+		Arrays.fill(expected, true);
+		Arrays.fill(actual, false);
+		// check if all edges are correct
+		for(Edge<Integer> e: edge) {
+			if(subVertex.contains(e.getOpposingVertex(added))) {
+				actual[e.getOpposingVertex(added)] = true;
+			}
+		}
+		assertArrayEquals(actual,expected); // test 
 	}
  
 	@Test
