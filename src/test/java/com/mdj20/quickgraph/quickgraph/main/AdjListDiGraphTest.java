76513c03f70d2cf75de6042a3798c43b25756056
Matthew Jeffreys <mdj20@pitt.edu>
2018-02-12 01:29:44 -0500

    AdjListGtraphTest removed print statements.

--- src/test/java/com/mdj20/quickgraph/quickgraph/main/AdjListDiGraphTest.java
+++ src/test/java/com/mdj20/quickgraph/quickgraph/main/AdjListDiGraphTest.java
@@ -249,7 +249,6 @@ public class AdjListDiGraphTest {
 		int nEdges = completeNEdges(nVerts);
 		HashMap<Integer,ArrayList<Integer>> connectedMap = completeGraphEndpointMap(nVerts);
 		AdjListDiGraph<Integer> testGraph = createCompleteGraph(nVerts);
-		//System.out.println(testGraph.getEdges().size()+" "+nEdges);
 		assertTrue(testGraph.getEdges().size()==nEdges);
 		ArrayList<DirectedEdge<Integer>> edges 
 			= new ArrayList<DirectedEdge<Integer>>(testGraph.getEdges());
